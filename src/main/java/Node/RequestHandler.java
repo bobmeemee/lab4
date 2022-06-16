758b2bd8166d9479806d01264fac89d6275500b2
iliB <56929828+bobmeemee@users.noreply.github.com>
2022-06-16 20:56:00 +0200

    edge case for 3 nodes in network and insert at edge high and low

--- src/main/java/Node/RequestHandler.java
+++ src/main/java/Node/RequestHandler.java
@@ -47,22 +47,23 @@ public class RequestHandler extends Thread {
                     System.out.println("[NODE]: new next " + this.node.getNextID());
                     System.out.println("[NODE]: new previous " + this.node.getPreviousID());
                     // sender is new next
-                } else if((senderID < this.node.getNextID()) && (senderID > this.node.getNodeID())
-                        || (this.node.getNextID() == this.node.getPreviousID() && this.node.getPreviousID()<this.node.getNodeID()) ) {
+                } else if(((senderID < this.node.getNextID()) && (senderID > this.node.getNodeID()))
+                        || (this.node.getNextID() == this.node.getPreviousID() && this.node.getPreviousID()<this.node.getNodeID())
+                        || (this.node.getNextID() < this.node.getNodeID() && senderID < this.node.getNextID())) {
                     this.node.setNextID(senderID);
                     response = new InsertAsPreviousMessage(this.node.getNodeID());
                     sendUnicastResponse = true;
                     System.out.println("[NODE]: new next " + this.node.getNextID());
                     System.out.println("[NODE]: previous " + this.node.getPreviousID());
                     // sender is new prev
-                } else if(senderID > this.node.getPreviousID() && senderID < this.node.getNodeID()
-                        || (this.node.getNextID() == this.node.getPreviousID() && this.node.getNextID() > this.node.getNodeID())) {
+                } else if((senderID > this.node.getPreviousID() && senderID < this.node.getNodeID())
+                        || (this.node.getNextID() == this.node.getPreviousID() && this.node.getNextID() > this.node.getNodeID())
+                        || (this.node.getPreviousID() > this.node.getNodeID()) && senderID < this.node.getNodeID()) {
                     this.node.setPreviousID(senderID);
                     response = new InsertAsNextMessage(this.node.getNodeID());
                     sendUnicastResponse = true;
                     System.out.println("[NODE]: next " + this.node.getNextID());
                     System.out.println("[NODE]: new previous " + this.node.getPreviousID());
-
                 }
                 break;
             case "LeavingNetworkMessage":
